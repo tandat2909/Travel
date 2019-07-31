cbf8242f785f0bf814cc1d0b947b9ff5a1cf6f71
tandat2909 <1851050032dat@ou.edu.vn>
2019-07-31 20:01:13 +0700

    v1

---
+++ js/animation-SP.js
@@ -0,0 +1,47 @@
+//============= animation-spECIAL DEALS================================
+       
+
+var sp_items = document.getElementsByClassName('item'); 
+        var sp_items_lenght = sp_items.length ; // so phan tu item
+        var width=0; // chieu dai cua item
+
+        function add_animation (width) 
+        {
+          
+           for(var i = 0 ; i < 7 ; i++)
+            {
+                sp_items[i].classList.add('animation-sp');
+                sp_items[i].style.transform = 'translateX(' + width + '%)';
+            }
+        }
+
+
+
+        function right_sp()
+        {
+            width -=110 ;
+            if(width >= -(sp_items_lenght -4)*110) // (sp_items_lenght -4) = so item an chua hien thi ; 4 là số item được hiển thị mặc định "action" 
+                          
+                add_animation (width);
+              
+            else
+            {
+                width = 0;
+                add_animation(width);
+            }
+        }
+
+        function left_sp()
+        {
+           
+            if(width == 0 )
+            {
+                width = -(sp_items_lenght -4)*110;
+                add_animation(width);
+            }
+            else
+            {
+                width += 110;
+                add_animation(width);
+            }
+        }
--- js/slide.js
+++ js/slide.js
@@ -2,52 +2,73 @@
 	var max = document.getElementsByClassName('run').length;
 	var min = 0;
 
+	function itemID(stt)
+	{
+		return document.getElementById(stt)
+	}
+	
 	function prev () {
 		stt--;
 		if(stt < min)
 		{
 			
-			document.getElementById(stt+1).style.display="none";
+			itemID(stt+1).style.display="none";
 			stt = max;
-			document.getElementById(stt).style.display="block";
-			//document.getElementById(stt).attr('class',animation);
+			itemID(stt).style.display="block";
 		}
 		else
 		{
-			document.getElementById(stt+1).style.display="none";
+			itemID(stt+1).style.display="none";
 			if(stt-1 == -1)
-			document.getElementById(0).style.display="block";
+			itemID(0).style.display="block";
 			else
-			document.getElementById(stt).style.display="block";
-			//document.getElementById(stt).attr('class',animation);
+			itemID(stt).style.display="block";
+			
 			
 		}
 		
 		console.log('item prev: ' + stt  );
 	}
+	
+	
 
 	function next () {
 		stt++;
 		if(stt > max)
 		{
 			
-			document.getElementById(stt-1).style.display="none";
+			itemID(stt-1).style.display="none";
 			stt = min;
-			document.getElementById(stt).style.display="block";
-			//document.getElementById(stt).attr('class',animation);
+			itemID(stt).style.display="block";
+			
 		}
 		else
 		{
-			document.getElementById(stt-1).style.display="none";
+			itemID(stt-1).style.display="none";
 			if(stt + 1 == max + 1)
-			document.getElementById(max).style.display="block";
+			itemID(max).style.display="block";
 			else
-			document.getElementById(stt).style.display="block";
-			//document.getElementById(stt).attr('class',animation);
+			itemID(stt).style.display="block";
+			
 		
 		}
 		
 		console.log('item next: ' + stt  );
 	}
 
-	setInterval(next().click, 4000);
+	setInterval(function(){next();}, 4000);
+
+
+	
+
+
+	
+/*
+	var a = document.getElementById("uhchatboz")
+	a.onclick= function (){
+		if(a.style.bottom == "-320px")
+			a.style.bottom = "0px";
+		else
+			a.style.bottom = "-320px";
+	}
+	*/
