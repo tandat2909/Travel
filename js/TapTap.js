3c32f34de999d643a89d31646bf7e2a0a4c5c0dc
TuDL14 <52564112+TuDL14@users.noreply.github.com>
2019-08-07 14:11:53 +0700

    js tap 10day new

---
+++ js/TapTap.js
@@ -0,0 +1,15 @@
+
+function opentap(e, tapId){
+	var i, tapcontents, tablink;
+	tapcontents = document.getElementsByClassName("TapContent");
+	for (i = 0; i < tapcontents.length; i++)
+		tapcontents[i].style.display = 'none';
+	tapcontents[tapId].style.display = 'block';
+    tapcontents[tapId].classList.add('animated', 'fadeInUp');    
+
+	tablinks = document.getElementsByClassName("tap-link");
+  	for (i = 0; i < tablinks.length; i++){
+    	tablinks[i].className = tablinks[i].className.replace(" active", " ");
+  	}
+	e.currentTarget.className += " active";
+}
