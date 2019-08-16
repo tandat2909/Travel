35b79fefe04d05aedecf38ce09eacc874dc1ff51
tandat2909 <1851050032dat@ou.edu.vn>
2019-08-16 16:34:50 +0700

    sdf

--- js/TapTap.js
+++ js/TapTap.js
@@ -1,9 +1,11 @@
-
 function opentap(e, tapId){
 	var i, tapcontents, tablink;
 	tapcontents = document.getElementsByClassName("TapContent");
+
 	for (i = 0; i < tapcontents.length; i++)
 		tapcontents[i].style.display = 'none';
+
+
 	tapcontents[tapId].style.display = 'block';
     tapcontents[tapId].classList.add('animated', 'fadeInUp');    
 
